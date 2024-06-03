2c33b255f153169242ea60c8cf1d3313ff9e1978
lara-11 <laradesigns11@gmail.com>
2024-06-03 17:22:07 -0500

    Menu, Hover, & Section 4 Edits

--- influx/app/page.tsx
+++ influx/app/page.tsx
@@ -1,38 +1,44 @@
 import React from 'react';
-import './style.css'; // Adjust the path if needed
+import './style.css'; // Import your CSS file
 
-const ROSPoweredUGVs: React.FC = () => {
-  return (
-    <div className="agilex-flex-container2">
-      <div className="agilex-powered-box1">
-        <h1 className="agilex-powered-h1">ROS Powered UGVs</h1>
-        <div className="agilex-powered-line"></div>
+const Page: React.FC = () => {
+    return (
+        <div>
+            {/* InFlux NAV Bar */}
+            <header>
+                <nav id="navbar" data-spy="affix">
+                    <ul className="nav-logo">
+                        <a href="#">
+                            <img src="img/InFlux-Logo-Black.png" alt="InFlux Technologies" className="logo" />
+                        </a>
+                    </ul>
 
-        <p className="agile-powered-p">
-          AgileX offers a full line up of mobile ROS powered UGV/AGVs for
-          industrial and research applications. These platforms come in a variety
-          of sizes and drive options for; 4 wheel drive, omni-directional drive,
-          Ackerman steering and tracked options with payloads of up to 150kgs. The
-          AgileX development kits expand platform capabilities to include:
-          autonomous driving, path planning, 3D point cloud mapping and obstacle
-          avoidance.
-        </p>
-      </div>
+                    <ul className="nav-menu">
+                        <li className="nav-item">
+                            <a href="#" className="nav-link">Events</a>
+                            <span className="arrow">&#x2304;</span>
+                        </li>
+                        <li className="nav-item">
+                            <a href="#" className="nav-link">Partners</a>
+                            <span className="arrow">&#x2304;</span>
+                        </li>
+                        {/* Add more list items */}
+                    </ul>
 
-      <div className="agilex-powered-box2">
-        <figure>
-          <iframe
-            src="https://www.youtube.com/embed/g-oNjPtah8g"
-            className="agilex-powered-video"
-            title="YouTube video player"
-            frameBorder="0"
-            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-            allowFullScreen
-          ></iframe>
-        </figure>
-      </div>
-    </div>
-  );
-};
+                    {/* Add more navigation elements */}
+                </nav>
+            </header>
 
-export default ROSPoweredUGVs;
+            {/* InFlux Body */}
+            <main>
+                {/* Add your container elements */}
+            </main>
+
+            <footer>
+                {/* Add footer content */}
+            </footer>
+        </div>
+    );
+}
+
+export default Page;
