0676ff0be01cc178dcdf1dc7ac528e3d7c1aa8aa
lara-11 <laradesigns11@gmail.com>
2024-06-06 15:40:30 -0500

    Search Icon Add / Arrow Menu 2 Removed

--- influx/app/page.tsx
+++ influx/app/page.tsx
@@ -1,44 +1,66 @@
 import React from 'react';
 import './style.css'; // Import your CSS file
 import './nav.css'; // Import your CSS file
+import './script.js'; // Import your JavaScript file
 
-const Page: React.FC = () => {
+
+ const Page: React.FC = () => {
     return (
-        <div>
-            {/* InFlux NAV Bar */}
-            <header>
-                <nav id="navbar" data-spy="affix">
-                    <ul className="nav-logo">
-                        <a href="#">
-                            <img src="img/InFlux-Logo-Black.png" alt="InFlux Technologies" className="logo" />
-                        </a>
-                    </ul>
 
-                    <ul className="nav-menu">
-                        <li className="nav-item">
-                            <a href="#" className="nav-link">Events</a>
-                            <span className="arrow">&#x2304;</span>
-                        </li>
-                        <li className="nav-item">
-                            <a href="#" className="nav-link">Partners</a>
-                            <span className="arrow">&#x2304;</span>
-                        </li>
-                        {/* Add more list items */}
-                    </ul>
+<header>
+        <nav id="navbar" data-spy="affix">
+            <ul className="nav-logo">
+                <a href="#">
+                    <img src={InFluxLogoBlack} alt="InFlux Technologies" className="logo" />
+                </a>
+            </ul>
 
-                    {/* Add more navigation elements */}
-                </nav>
-            </header>
+            <ul className="nav-menu">
+                <li className="nav-item">
+                    <a href="#" className="nav-link">Events</a>
+                    <span className="arrow">&#x2304;</span>
+                </li>
+                <li className="nav-item">
+                    <a href="#" className="nav-link">Partners</a>
+                    <span className="arrow">&#x2304;</span>
+                </li>
+                <li className="nav-item">
+                    <a href="#" className="nav-link">About Us</a>
+                    <span className="arrow">&#x2304;</span>
+                </li>
+                <li className="nav-item">
+                    <a href="#" className="nav-link">Additional Projects</a>
+                    <span className="arrow">&#x2304;</span>
+                </li>
+                <li className="nav-item">
+                    <a href="#" className="nav-link">Contact Us</a>
+                    <span className="arrow">&#x2304;</span>
+                </li>
+            </ul>
 
-            {/* InFlux Body */}
-            <main>
-                {/* Add your container elements */}
-            </main>
+            <ul className="nav-menu2">
+                <li className="nav-item">
+                    <a href="#" className="nav-link">List Item 1</a>
+                    <span className="arrow">&#x2304;</span>
+                </li>
+                <li className="nav-item">
+                    <a href="#" className="nav-link">List Item 2</a>
+                    <span className="arrow">&#x2304;</span>
+                </li>
+                <li className="nav-item">
+                    <a href="#" className="nav-link">List Item 3</a>
+                    <span className="arrow">&#x2304;</span>
+                </li>
+            </ul>
 
-            <footer>
-                {/* Add footer content */}
-            </footer>
-        </div>
+            <div className="hamburger">
+                <span className="bar"></span>
+                <span className="bar"></span>
+                <span className="bar"></span>
+            <div/>
+        </nav>
+    <header/>
+
     );
 }
 
